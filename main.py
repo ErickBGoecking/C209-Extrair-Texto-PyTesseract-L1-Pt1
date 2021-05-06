100f5c6bd0402383b09e289be908c21fa92dc9b4
ErickBGoecking <60221860+ErickBGoecking@users.noreply.github.com>
2021-05-05 22:03:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 img = cv2.imread("030.PNG")
 text = pytesseract.image_to_string(img)
 print(text)
+
+# Erick Braun Goecking - 1265 - GEC
+# Paola de Souza Freitas - 1474 - GEC
